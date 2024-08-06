ae1042ddcecbbe7ac6a4f310d292fb1510b79b90
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-06 14:34:01 -0300

    Criando classeTarefaModel

---
+++ src/ExerciciosResolvidoEmAula/Tarefa.java
@@ -0,0 +1,16 @@
+package ExerciciosResolvidoEmAula;
+
+public class Tarefa {
+
+    //Atributo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+}
